f96983bc1dd214eb5ad2ed8f77297eb8c1f3f43f
s3cur3 <s3cur3@gmail.com>
2012-06-15 15:14:18 -0500

    Fixes borked files

---
+++ src/edu/cs/illinois/cogcomp/hadoopinterface/CuratorMapper.java
@@ -0,0 +1,41 @@
+
+
+/**
+ * Provides the map() method to Hadoop's MapReduce.
+ * Called by HadoopInterface
+ * 
+ * @author Tyler A. Young
+ * @author Lisa Bao
+ */
+
+package edu.cs.illinois.cogcomp.hadoopinterface;
+
+import org.apache.hadoop.io.ObjectWritable;
+
+public class CuratorMapper extends MapReduceBase implements
+        Mapper<LongWritable, LongWritable, BooleanWritable, LongWritable> {
+
+    /**
+     * The map method in a map/reduce cycle. All nodes in the Hadoop
+     * job cluster run this on their own portions of the input.
+     * @param offset samples starting from the (offset+1)th sample.
+     * @param size the number of samples for this map
+     * @param out output {true->numInside, false->numOutside}
+     * @param reporter TODO: Where does this come from, and how do we use it?
+     */
+    public void map(LongWritable offset,
+                    LongWritable size,
+                    OutputCollector<BooleanWritable, LongWritable> out,
+                    Reporter reporter) throws IOException {
+
+        reporter.setStatus("Beginning map phase.");
+
+        // Output the map results
+        // TODO: Actually map stuff.
+        String key = new String("0xdeadbeef"); // The document's hash (unique)
+        String value = new String("This is my document text.");
+        out.collect(new ObjectWritable( key ), new ObjectWritable( value ));
+    }
+
+
+}
