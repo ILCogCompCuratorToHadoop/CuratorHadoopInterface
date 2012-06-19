92b2a81b6c215965ab5137549f72a27ee4253091
lisa <lisa@tremontaine>
2012-06-19 16:57:33 -0500

    Merged versions of Mapper and Reducer.

--- src/edu/cs/illinois/cogcomp/hadoopinterface/CuratorMapper.java
+++ src/edu/cs/illinois/cogcomp/hadoopinterface/CuratorMapper.java
@@ -3,7 +3,7 @@
  * Called by HadoopInterface
  * 
  * @author Tyler A. Young
- * @author Lisa Bao
+ * @author Lisa Y. Bao
  */
 
 package edu.cs.illinois.cogcomp.hadoopinterface;
@@ -17,6 +17,7 @@ import java.io.IOException;
 
 public class CuratorMapper extends Mapper<Text, Record, Text, Record> {
 
+
     /**
      * The map method in a map/reduce cycle. All nodes in the Hadoop
      * job cluster run this on their own portions of the input.
@@ -24,35 +25,31 @@ public class CuratorMapper extends Mapper<Text, Record, Text, Record> {
      * @param inValue = value
      * @param context The configuration context
      */
-    public void map( Text inKey,
-                     Record inValue,
-                     Context context ) throws IOException {
-
+    public void map( Text inKey, 
+                     Record inValue, 
+                     Context context) throws IOException {
+    
         HadoopInterface.logger.logStatus( "Beginning map phase.\n"
-                                          + "\t\tGot input key " + inKey.toString()
-                                          + "\n\t\tand input value "
+                                          + "Attempting to acces vars." );
+        HadoopInterface.logger.logStatus( "\tGot input key " + inKey.toString()
+                                          + "\n\tand input value: "
                                           + inValue.toString() );
 
 
-        // Output the map results
-
-        // TODO
-        // 1. write our own Record class
-        // 2. modify params of class/method
-        // 3. add writeable/comparable interfaces to id and record objs
-        // 4. change id type from String to "text"?
+        // TODO write our own Record class, w/ writeable/comparable interface
 
 
-        // TODO print out input that has been passed in
+    	/*transform input to output as (key, value) = (hash ID, Record)
+          String key = id;
+          Record value = record;
 
+          String key = new String("0xdeadbeef"); // The document's hash (unique)
+          String value = new String("This is my document text.");
+        */
         
 
-    	// transform input to output as (key, value) = (hash ID, Record)
-        //String key = id;
-        //Record value = record;
-
-        //String key = new String("0xdeadbeef"); // The document's hash (unique)
-        //String value = new String("This is my document text.");
+        // context.write replaces out.collect()
+        context.write(inKey, inValue);
         
     }
 
--- src/edu/cs/illinois/cogcomp/hadoopinterface/CuratorReducer.java
+++ src/edu/cs/illinois/cogcomp/hadoopinterface/CuratorReducer.java
@@ -7,7 +7,8 @@ import org.apache.hadoop.mapreduce.Reducer;
 import java.io.IOException;
 
 /**
- * @author Tyler Young
+ * @author Tyler A. Young
+ * @author Lisa Y. Bao
  */
 public class CuratorReducer extends Reducer<Text, Record, Text, Record> {
     /**
@@ -16,23 +17,31 @@ public class CuratorReducer extends Reducer<Text, Record, Text, Record> {
      * @param inValue
      * @param context
      */
-    public void reduce( Text inKey, Record inValue, Context context)
-            throws IOException {
+    public void reduce( Text inKey, 
+                        Record inValue, 
+                        Context context ) throws IOException {
 
-        // TODO write input document to HDFS
-    	//Path file = ; // TODO pull filepath from Record object
+        // write input document to HDFS
+        Path file = inValue.getPath(); // pulls Hadoop-HDFS filepath from Record object
         
-	    // TODO while loop, wait for output in appropriate directory to "magically" appear
+	    // while loop, wait for output in appropriate directory to "magically" appear
         // (thanks to the local Curator instance)
         boolean done = false;
-
-        /*
+        Path output = null;
         while (!done) {
-            Path output = ; // TODO check for Curator output in local dir
-            if (output == ) {
+            try {
+                // attempts to get java.io path to curator output, if it exists
+                output = FileSystems.getDefault().getPath("out", "curator_out.txt");
+            } catch (IOException e) {
+                System.out.println("Waiting on Curator output");
+            } 
+            if (output != null) {
                 done = true;
-                // TODO do we need to process this Curator output before close()?
+                // pass Curator output back to Hadoop as Record
+                BufferReader reader = Files.newBufferedReader(output, StandardCharsets.UTF_8);
+                Record curatorRecord = new Record(reader); // TODO does Record allow optional params in creation?
+                context.write(inKey, curatorRecord);
             }
-        }              */
+        }              
     }
 }
