4460c08dd2128cf6e9cbd609526f17aa3f5c863c
lisa <lisa@tremontaine>
2012-07-25 13:57:00 -0500

    Changed CuratorKillerReducer to while-loop over all Curator processes (untested).

--- src/edu/illinois/cs/cogcomp/hadoopinterface/infrastructure/CuratorKillerReducer.java
+++ src/edu/illinois/cs/cogcomp/hadoopinterface/infrastructure/CuratorKillerReducer.java
@@ -28,15 +28,31 @@ public class CuratorKillerReducer extends
         // Command to do this: "jps -l | grep edu.illinois.cs.cogcomp.curator.CuratorServer | cut -d ' ' -f 1 | xargs -n1 kill"
         //      See here: http://stackoverflow.com/questions/2131874/shell-script-to-stop-a-java-program
 
-        String cmd = "jps -l | " + // Get the list of all running Java processes
+        /* Preserved in case I mess things up below:
+        String curator = "jps -l | " + // Get the list of all running Java processes
                 // Select the line for the Curator server
                 "grep edu.illinois.cs.cogcomp.curator.CuratorServer | " +
                 // Split the line on spaces
                 "cut -d ' ' -f 1 | " +
-                // Send the first element of the split line (i.e., the process
-                // ID) to the kill command
+                // Send the first element of the split line (i.e., the process ID) to the kill command
                 "xargs -n1 kill";
+        Runtime.getRuntime().exec(curator);
+        */
 
-        Runtime.getRuntime().exec( cmd );
+        while (true) { //TODO triple-check that this loop can't get stuck in infinity...
+            String tool = "jps -l | " + // Get the list of all running Java processes
+                // Select the first matching process
+                "grep edu.illinois.cs.cogcomp | head -n 1 | " +
+                // Split the line on spaces
+                "cut -d ' ' -f 1 | " +
+                // Send the first element of the split line (i.e., the process ID) to the kill command
+                "xargs -n1 kill";
+            try {
+                Runtime.getRuntime().exec(tool);
+            } catch (RuntimeException e) {
+                break; // stop when grep fails to find a matching process
+            }
+        }
+
     }
 }
