201c2037e91edd43c241b54b88b34f9642e4a532
s3cur3 <s3cur3@gmail.com>
2012-08-01 11:12:47 -0500

    Fixed cleanup script to work with 32 installations of Curator

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -5,14 +5,12 @@
 # It is really, really specific to the implementation we're
 # using on the Altocumulus cluster at UIUC.
 
-cd /project/cogcomp
-
 # For each Curator directory . . .
 # Delete the "Curator lock" file
 CURATOR_LOCK="CURATOR_IS_IN_USE"
-cd curator-0.6.9_0
-rm $CURATOR_LOCK
-cd ../curator-0.6.9_1
-rm $CURATOR_LOCK
-cd ../curator-0.6.9_2
-rm $CURATOR_LOCK
+BASE=/project/cogcomp/curator-0.6.9_
+for i in {1..32}
+do
+    CMD="rm $BASE$i/$CURATOR_LOCK"
+    echo "Running command $CMD"
+done
