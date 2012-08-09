8f28fdc2b414c179b3eb04a0888053cde2389c51
lisa <lisa@tremontaine>
2012-08-09 15:38:11 -0500

    Forgot to add directory_restructure.sh to Git...

---
+++ scripts/directory_restructure.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# A script to rearrange output from the Ant build of the CuratorHadoopInterface into the recommended directory structure specified in the README.
+# You must run this script from its original location in the scripts directory!
+
+echo -e "Copying Ant output into recommended directory structure..."
+
+cp ../out/artifacts/Jar/CuratorHadoopInterface.jar ../../CuratorHadoopInterface
+cp ../out/artifacts/JobHandler/JobHandler.jar ../../JobHandler
+cp -R ../lib ../../JobHandler
+cp -R ../scripts ../../JobHandler
+
+echo -e "Directory restructuring is complete!"
